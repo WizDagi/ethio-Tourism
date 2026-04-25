7bfafabe4eac2952c09e877dc5288ef708265e31
WizDazi <dndagi2424@gmail.com>
2026-04-25 15:43:03 -0700

    Add interactive features to HomePage: Quiz, Marquee, Testimonials, Floating Menu

--- frontend/src/features/home/components/HomePage.tsx
+++ frontend/src/features/home/components/HomePage.tsx
@@ -4,14 +4,22 @@ import { WhyEthiopia } from './WhyEthiopia';
 import { ExperiencesSection } from './ExperiencesSection';
 import { TravelTipsSection } from './TravelTipsSection';
 import { NewsletterSection } from './NewsletterSection';
+import { TripQuizSection } from './TripQuizSection';
+import { TestimonialsSection } from './TestimonialsSection';
+import { GalleryMarqueeSection } from './GalleryMarqueeSection';
+import { FloatingActionMenu } from './FloatingActionMenu';
 
 export const HomePage = () => {
   return (
-    <main className="w-full min-h-screen bg-background">
+    <main className="w-full min-h-screen bg-background relative">
+      <FloatingActionMenu />
       <HeroSlider />
+      <TripQuizSection />
       <WhyEthiopia />
+      <GalleryMarqueeSection />
       <DestinationGrid />
       <ExperiencesSection />
+      <TestimonialsSection />
       <TravelTipsSection />
       <NewsletterSection />
     </main>
